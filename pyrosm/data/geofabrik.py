94e23b6536a15b280036b86e131de87361728156
Rémy Greinhofer <remy.greinhofer@gmail.com>
2026-08-02 13:31:40 -0500

    Make Spanish Communities Lowercase
    Converts the names of the Spanish communities to lowercase.
    
    Signed-off-by: Rémy Greinhofer <remy.greinhofer@gmail.com>

--- pyrosm/data/geofabrik.py
+++ pyrosm/data/geofabrik.py
@@ -667,6 +667,54 @@ class Canada:
         return self.available
 
 
+class Spain:
+    regions = [
+        "andalucía",
+        "aragón",
+        "asturias",
+        "cantabria",
+        "castilla-la mancha",
+        "castilla y león",
+        "cataluña",
+        "ceuta",
+        "extremadura",
+        "galicia",
+        "islas baleares",
+        "la rioja",
+        "madrid",
+        "melilla",
+        "murcia",
+        "navarra",
+        "país vasco",
+        "valencia",
+    ]
+
+    available = regions
+    available.sort()
+
+    country = {
+        "name": "spain" + suffix,
+        "url": URL + europe_url + "spain" + suffix,
+    }
+
+    # Create data sources
+    _sources = {
+        region: {
+            "name": region.replace("_", "-") + suffix,
+            "url": URL + spain_url + region.replace("_", "-") + suffix,
+        }
+        for region in regions
+    }
+
+    __dict__ = _sources
+
+    def __getattr__(self, name):
+        return self.__dict__[name]
+
+    def __call__(self):
+        return self.available
+
+
 class Brazil:
     regions = ["centro_oeste", "nordeste", "norte", "sudeste", "sul"]
     available = regions
@@ -918,13 +966,14 @@ class CentralAmerica:
 class Europe:
     # Country specific subregions
     france = France()
-    great_britain = GreatBritain()
-    united_kingdom = UnitedKingdom()
-    italy = Italy()
-    russia = Russia()
-    poland = Poland()
     germany = Germany()
+    great_britain = GreatBritain()
+    italy = Italy()
     netherlands = Netherlands()
+    poland = Poland()
+    russia = Russia()
+    spain = Spain()
+    united_kingdom = UnitedKingdom()
 
     regions = [
         "albania",
@@ -1212,54 +1261,6 @@ class SubRegions:
         self.usa = USA()
 
         self.available = {name: self.__dict__[name].available for name in self.regions}
-
-    def __getattr__(self, name):
-        return self.__dict__[name]
-
-    def __call__(self):
-        return self.available
-
-
-class Spain:
-    regions = [
-        "Andalucía",
-        "Aragón",
-        "Asturias",
-        "Cantabria",
-        "Castilla-La Mancha",
-        "Castilla y León",
-        "Cataluña",
-        "Ceuta",
-        "Extremadura",
-        "Galicia",
-        "Islas Baleares",
-        "La Rioja",
-        "Madrid",
-        "Melilla",
-        "Murcia",
-        "Navarra",
-        "País Vasco",
-        "Valencia",
-    ]
-
-    available = regions
-    available.sort()
-
-    country = {
-        "name": "spain" + suffix,
-        "url": URL + europe_url + "spain" + suffix,
-    }
-
-    # Create data sources
-    _sources = {
-        region: {
-            "name": region.replace("_", "-") + suffix,
-            "url": URL + spain_url + region.replace("_", "-") + suffix,
-        }
-        for region in regions
-    }
-
-    __dict__ = _sources
 
     def __getattr__(self, name):
         return self.__dict__[name]
